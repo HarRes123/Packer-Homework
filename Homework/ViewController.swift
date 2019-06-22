b8238c1c1e94ddd81e5a3b732ba548d38bcd55bf
Harrison Resnick <haresnick@packer.edu>
2019-06-22 16:35:29 -0400

    Initial Commit

---
+++ Homework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework
+//
+//  Created by Harrison Resnick on 6/22/19.
+//  Copyright © 2019 Et Cetera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
