b0bb28240b163ec0f7fce474bf314c8b798fe69e
Harrison Resnick <haresnick@packer.edu>
2019-06-23 16:05:35 -0400

    Saves to database

--- Homework/SurveyViewController.swift
+++ Homework/SurveyViewController.swift
@@ -7,18 +7,50 @@
 //
 
 import UIKit
+import Firebase
 
 class SurveyViewController: UIViewController {
 
+    @IBOutlet weak var firstQuestion: UITextField!
+    @IBOutlet weak var secondQuestion: UITextField!
+    @IBOutlet weak var thirdQuestion: UITextField!
+    @IBOutlet weak var labelMessage: UILabel!
+    
+    var refResponse: DatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        refResponse = Database.database().reference().child("response")
+        labelMessage.text = ""
 
         // Do any additional setup after loading the view.
+    }
+    
+    func addResponse() {
+        
+        let key = refResponse.childByAutoId().key
+        
+        let responses = [
+                         "displayName": Auth.auth().currentUser?.displayName,
+                         "emailAddress": Auth.auth().currentUser?.email,
+                         "firstQuestion": firstQuestion.text! as String,
+                         "secondQuestion": secondQuestion.text! as String,
+                         "thirdQuestion": thirdQuestion.text! as String
+        ]
+        
+        refResponse.child(key!).setValue(responses)
+        labelMessage.text = "Added"
     }
 
     override func viewWillAppear(_ animated: Bool) {
          self.navigationController?.isNavigationBarHidden = false
     }
+    
+    @IBAction func saveButton(_ sender: UIButton) {
+        addResponse()
+    }
+    
     /*
     // MARK: - Navigation
 
