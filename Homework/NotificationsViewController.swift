69c51367b10c32a77c25574ba754069a51f593c7
Harrison Resnick <haresnick@packer.edu>
2019-07-25 10:40:36 -0400

    Cancel button improvements

--- Homework/NotificationsViewController.swift
+++ Homework/NotificationsViewController.swift
@@ -43,6 +43,8 @@ class NotificationsViewController: UIViewController {
         let alert = UIAlertController(title: alertNotification, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         self.present(alert, animated: true)
+
+        self.checkForNotifications()
         
     }
     
@@ -57,8 +59,7 @@ class NotificationsViewController: UIViewController {
     @IBAction func cancelNotification(_ sender: Any) {
         
         alertNotification = "Notification Canceled"
-        let center = UNUserNotificationCenter.current()
-        center.removeAllPendingNotificationRequests()
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
         presentPopOver()
     }
     
@@ -68,8 +69,35 @@ class NotificationsViewController: UIViewController {
         
     }
     
+    func checkForNotifications() {
+        
+        UNUserNotificationCenter.current().getPendingNotificationRequests { (notifications) in
+            print(notifications)
+
+            DispatchQueue.main.async {
+                if notifications != [] {
+                    
+                    self.cancelOutlet?.isEnabled = true
+                    self.cancelOutlet.backgroundColor = .appleBlue()
+                    
+                } else {
+                    
+                    self.cancelOutlet?.isEnabled = false
+                    self.cancelOutlet.backgroundColor = .gray
+                    
+                }
+                
+            }
+        }
+    }
+
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.isNavigationBarHidden = false
+        
+    
+        self.checkForNotifications()
+        
+
     }
     
     func scheduleNotification() {
@@ -99,7 +127,7 @@ class NotificationsViewController: UIViewController {
         //let dateComponent2 = datePicker.calendar.dateComponents([.day, .hour, .minute], from: datePicker.date)
         let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponent, repeats: shouldRepeat)
         
-        let notificationReq = UNNotificationRequest(identifier: "identifier", content: content, trigger: trigger)
+        let notificationReq = UNNotificationRequest(identifier: "completeSurvey", content: content, trigger: trigger)
         UNUserNotificationCenter.current().add(notificationReq, withCompletionHandler: nil)
     }
 
@@ -113,4 +141,10 @@ class NotificationsViewController: UIViewController {
     }
     */
 
+}
+
+extension UIColor {
+    static func appleBlue() -> UIColor {
+        return UIColor(red: 14.0/255, green: 122.0/255, blue: 254.0/255, alpha: 1.0)
+    }
 }
